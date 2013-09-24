f5c103153fa8c49356ddc1c18ec361e42f2ed240
2012060100030 <1728368345@qq.com>
2013-09-24 11:11:22 +0800

    helloworld

---
+++ helloworld/helloworld/helloworld.cpp
@@ -0,0 +1,18 @@
+// helloworld.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello World!I'm renhuilei!"<<endl;
+	return 0;
+}
